5dd88d08528ab5b95eb51e863209f87457ce6cb1
Trainee 29 <umesh.more@raweng.com>
2020-12-03 15:55:17 +0530

    used fetch

--- src/data.js
+++ src/data.js
@@ -1,6 +1,11 @@
 const url = "https://umeshmore45.github.io/files/data.json";
 
+// export const data = (async () => {
+//   const get = await axios.get(url);
+//   return get.data;
+// })();
+
 export const data = (async () => {
-  const get = await axios.get(url);
-  return get.data;
+  const get = await fetch(url);
+  return get.json();
 })();
